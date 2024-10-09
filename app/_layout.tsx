a2fc4bcc89c7d1269ea477faf21f08d9cade0ae7
Felix <felavalos@gmail.com>
2024-10-08 19:16:16 -0600

    Fix bundler mistake
    Change bundler to "metro". This property had a mistake
    Change scheme to "aora". Before was metacritic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Slot } from 'expo-router';
 
 export default function RootLayout() {
-  return (
-    <View style={ styles.container }>
-      <Text>RootLayout</Text>
-    </View>
-  );
+  return <Slot />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
